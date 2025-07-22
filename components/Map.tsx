0532e8b5eca8821fc5dbb227b1e916fbdc330e83
kdn0325 <dn10003@gmail.com>
2025-07-22 20:53:08 +0900

    feat: mapbox 마커 추가 및 클러스터 추가

---
+++ components/Map.tsx
@@ -0,0 +1,67 @@
+import Mapbox, {
+  Camera,
+  CircleLayer,
+  Images,
+  LocationPuck,
+  MapView,
+  ShapeSource,
+  StyleURL,
+  SymbolLayer,
+} from '@rnmapbox/maps';
+import { featureCollection, point } from '@turf/helpers';
+import pin from '~/assets/pin.png';
+import scooters from '~/data/scooters.json';
+
+Mapbox.setAccessToken(process.env.MAX_BOX_KEY || '');
+
+export default function Map() {
+  // 올바른 GeoJSON 포인트 배열 생성
+  const points = scooters.map((scooter) => point([scooter.long, scooter.lat]));
+
+  return (
+    <MapView style={{ flex: 1 }} styleURL={StyleURL.Dark}>
+      <Camera followZoomLevel={16} followUserLocation />
+      {/* <LocationPuck puckBearingEnabled puckBearing="heading" pulsing={{ isEnabled: true }} /> */}
+
+      <ShapeSource
+        id="scooters"
+        cluster
+        shape={featureCollection(points)}
+        onPress={(e) => console.log(JSON.stringify(e, null, 2))}>
+        <SymbolLayer
+          id="clusters-count"
+          style={{
+            textField: ['get', 'point_count'],
+            textSize: 18,
+            textColor: '#ffffff',
+            textPitchAlignment: 'map',
+          }}
+        />
+        <CircleLayer
+          id="clusters"
+          belowLayerID="clusters-count"
+          filter={['has', 'point_count']}
+          style={{
+            circlePitchAlignment: 'map',
+            circleColor: '#42E100',
+            circleRadius: 20,
+            circleOpacity: 1,
+            circleStrokeWidth: 2,
+            circleStrokeColor: 'white',
+          }}
+        />
+        <SymbolLayer
+          id="scooter-icons"
+          filter={['!', ['has', 'point_count']]}
+          style={{
+            iconImage: 'pin',
+            iconSize: 0.5,
+            iconAllowOverlap: true,
+            iconAnchor: 'bottom',
+          }}
+        />
+        <Images images={{ pin }} />
+      </ShapeSource>
+    </MapView>
+  );
+}

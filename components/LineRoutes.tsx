1b9bcbe3f44efeb5db87bb9f1b257d772f5fb798
kdn0325 <dn10003@gmail.com>
2025-07-23 17:15:05 +0900

    feat: Mapbox에서 LineString을 렌더링하는 LineRoutes 컴포넌트 구현

---
+++ components/LineRoutes.tsx
@@ -0,0 +1,31 @@
+import { StyleSheet } from 'react-native';
+import { LineLayer, ShapeSource } from '@rnmapbox/maps';
+import { Position } from '@rnmapbox/maps/lib/typescript/src/types/Position';
+
+export default function LineRoutes({ coordinates }: { coordinates: Position[] }) {
+  return (
+    <ShapeSource
+      id="routeSource"
+      shape={{
+        properties: {},
+        type: 'Feature',
+        geometry: {
+          type: 'LineString',
+          coordinates: coordinates,
+        },
+      }}>
+      <LineLayer
+        id="exampleLineLayer"
+        style={{
+          lineColor: '#42E100',
+          lineCap: 'round',
+          lineJoin: 'round',
+          lineWidth: 10,
+          lineDasharray: [1, 0],
+        }}
+      />
+    </ShapeSource>
+  );
+}
+
+const styles = StyleSheet.create({});

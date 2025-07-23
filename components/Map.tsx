23bed48eaadf9a28524fb52dd0001ca27c5ea1df
kdn0325 <dn10003@gmail.com>
2025-07-23 17:15:39 +0900

    feat: Map 컴포넌트에 ScooterMarker와 LineRoutes 컴포넌트 통합

--- components/Map.tsx
+++ components/Map.tsx
@@ -1,71 +1,21 @@
-import Mapbox, {
-  Camera,
-  CircleLayer,
-  Images,
-  MapView,
-  ShapeSource,
-  StyleURL,
-  SymbolLayer,
-} from '@rnmapbox/maps';
-import { featureCollection, point } from '@turf/helpers';
-import pin from '~/assets/pin.png';
-import scooters from '~/data/scooters.json';
+import Mapbox, { Camera, LocationPuck, MapView, StyleURL } from '@rnmapbox/maps';
 
-Mapbox.setAccessToken(process.env.MAX_BOX_KEY || '');
+import { useScooter } from '~/providers/ScooterProvider';
+
+import LineRoutes from './LineRoutes';
+import ScooterMarker from './ScooterMarker';
+
+Mapbox.setAccessToken(process.env.MAPBOX_TOKEN || '');
 
 export default function Map() {
-  const points = scooters.map((scooter) => point([scooter.long, scooter.lat], { id: scooter.id }));
-  const scootersFeatures = featureCollection(points);
+  const { directionCoordinate } = useScooter();
 
   return (
     <MapView style={{ flex: 1 }} styleURL={StyleURL.Dark}>
       <Camera followZoomLevel={10} followUserLocation />
-      <ShapeSource
-        id="scooters"
-        cluster={true}
-        clusterRadius={50}
-        shape={scootersFeatures}
-        onPress={(e) => {
-          e.features.forEach((feature) => {
-            console.log('Feature:', feature.properties);
-          });
-        }}>
-        <CircleLayer
-          id="clusters"
-          filter={['has', 'point_count']}
-          style={{
-            circleColor: '#42E100',
-            circleRadius: 20,
-            circleOpacity: 1,
-            circleStrokeWidth: 2,
-            circleStrokeColor: 'white',
-          }}
-        />
-        <SymbolLayer
-          id="clusters-count"
-          filter={['has', 'point_count']}
-          style={{
-            textField: ['get', 'point_count'],
-            textSize: 18,
-            textColor: '#ffffff',
-            textFont: ['DIN Offc Pro Medium', 'Arial Unicode MS Regular'],
-            textAllowOverlap: true,
-            textIgnorePlacement: true,
-          }}
-        />
-
-        <SymbolLayer
-          id="scooter-icons"
-          filter={['!', ['has', 'point_count']]}
-          style={{
-            iconImage: 'pin',
-            iconSize: 0.5,
-            iconAllowOverlap: true,
-            iconAnchor: 'bottom',
-          }}
-        />
-        <Images images={{ pin }} />
-      </ShapeSource>
+      <LocationPuck puckBearingEnabled puckBearing="heading" pulsing={{ isEnabled: true }} />
+      <ScooterMarker />
+      {directionCoordinate && <LineRoutes coordinates={directionCoordinate} />}
     </MapView>
   );
 }

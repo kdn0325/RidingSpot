3c7a83596eee76c928cbfa9521efa4c08b02b90c
kdn0325 <dn10003@gmail.com>
2025-07-24 15:49:48 +0900

    feat: Home 화면에 지도 및 선택된 스쿠터 바텀시트 추가

--- app/index.tsx
+++ app/index.tsx
@@ -1,12 +1,13 @@
 import { Stack } from 'expo-router';
 import Map from '~/components/Map';
-import ScooterProvider from '~/providers/ScooterProvider';
+import SelectedScooterSheet from '~/components/SelectedScooterSheet';
 
 export default function Home() {
   return (
-    <ScooterProvider>
-      <Stack.Screen options={{ title: 'Home' }} />
+    <>
+      <Stack.Screen options={{ title: 'Home', headerShown: false }} />
       <Map />
-    </ScooterProvider>
+      <SelectedScooterSheet />
+    </>
   );
 }

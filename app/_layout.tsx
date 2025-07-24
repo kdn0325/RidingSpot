8f4013d95041faee710d8e24e421ce69671fabbd
kdn0325 <dn10003@gmail.com>
2025-07-24 15:49:14 +0900

    feat: 글로벌 레이아웃에 GestureHandler와 ScooterProvider 적용

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,15 @@
 import { Stack } from 'expo-router';
+import { StatusBar } from 'react-native';
+import ScooterProvider from '~/providers/ScooterProvider';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
 export default function Layout() {
-  return <Stack />;
+  return (
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <ScooterProvider>
+        <Stack />
+        <StatusBar barStyle={'light-content'} />
+      </ScooterProvider>
+    </GestureHandlerRootView>
+  );
 }

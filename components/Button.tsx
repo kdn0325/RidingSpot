1736fed24a67c2d69c6473d42749b62dcf055294
kdn0325 <dn10003@gmail.com>
2025-07-24 17:32:46 +0900

    feat: disabled 스타일 추가

--- components/Button.tsx
+++ components/Button.tsx
@@ -5,37 +5,43 @@ type ButtonProps = {
   title?: string;
 } & TouchableOpacityProps;
 
-export const Button = forwardRef<View, ButtonProps>(({ title, ...touchableProps }, ref) => {
-  return (
-    <TouchableOpacity ref={ref} {...touchableProps} style={[styles.button, touchableProps.style]}>
-      <Text style={styles.buttonText}>{title}</Text>
-    </TouchableOpacity>
-  );
-});
-
-Button.displayName = 'Button';
+export const Button = forwardRef<View, ButtonProps>(
+  ({ title, disabled, style, ...touchableProps }, ref) => {
+    return (
+      <TouchableOpacity
+        ref={ref}
+        {...touchableProps}
+        disabled={disabled}
+        style={[styles.button, style, disabled && styles.buttonDisabledBackground]}>
+        <Text style={styles.buttonText}>{title}</Text>
+      </TouchableOpacity>
+    );
+  }
+);
 
 const styles = StyleSheet.create({
   button: {
     alignItems: 'center',
-    backgroundColor: '#6366F1',
+    backgroundColor: '#38C400',
     borderRadius: 24,
     elevation: 5,
     flexDirection: 'row',
     justifyContent: 'center',
     padding: 16,
     shadowColor: '#000',
-    shadowOffset: {
-      height: 2,
-      width: 0,
-    },
+    shadowOffset: { height: 2, width: 0 },
     shadowOpacity: 0.25,
     shadowRadius: 3.84,
+  },
+  buttonDisabledBackground: {
+    backgroundColor: '#A0A0A0',
+    elevation: 0,
+    shadowOpacity: 0,
   },
   buttonText: {
     color: '#FFFFFF',
     fontSize: 16,
-    fontWeight: '600',
+    fontWeight: '700',
     textAlign: 'center',
   },
 });

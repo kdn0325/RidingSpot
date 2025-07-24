a1373bc0d077b9b16984c3b49f0f604fb7555178
kdn0325 <dn10003@gmail.com>
2025-07-24 15:50:15 +0900

    feat: Mapbox 지도에 마커 및 경로 표시 기능 추가

--- components/Map.tsx
+++ components/Map.tsx
@@ -9,9 +9,13 @@ Mapbox.setAccessToken(process.env.MAPBOX_TOKEN || '');
 
 export default function Map() {
   const { directionCoordinate } = useScooter();
+  const customStyleURL = 'mapbox://styles/abelkim/cmdfs86wj03jd01r40yi091g2';
 
   return (
-    <MapView style={{ flex: 1 }} styleURL={StyleURL.Dark}>
+    <MapView
+      style={{ flex: 1 }}
+      styleURL={StyleURL.Dark}
+      onDidFinishLoadingMap={() => console.log('Map loaded')}>
       <Camera followZoomLevel={10} followUserLocation />
       <LocationPuck puckBearingEnabled puckBearing="heading" pulsing={{ isEnabled: true }} />
       <ScooterMarker />

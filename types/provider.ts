6b95ce3b25ec38df94803bb4b5a74afe641fa7b8
kdn0325 <dn10003@gmail.com>
2025-07-24 17:36:08 +0900

    chore: 기존 컨텍스트 타입 분리

---
+++ types/provider.ts
@@ -0,0 +1,12 @@
+import type { Scooter } from '~/types/Scooter';
+import { MapboxDirections } from './directionsResponse';
+
+export type ScooterContextType = {
+  selectedScooter?: Scooter;
+  setSelectedScooter: (scooter: Scooter) => void;
+  direction?: MapboxDirections;
+  directionCoordinate?: number[][];
+  duration?: number;
+  distance?: number;
+  isNearby?: boolean;
+};

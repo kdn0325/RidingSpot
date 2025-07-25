c4663840d22a96e6a4c37b531c83266118f4e5dd
kdn0325 <dn10003@gmail.com>
2025-07-25 16:39:35 +0900

    chore: Supabase 클라이언트 초기화 및 AsyncStorage 연동 설정

--- constant/index.ts
+++ constant/index.ts
@@ -1,3 +1,6 @@
 import Constants from 'expo-constants';
 
-export const MAPBOX_TOKEN = Constants.expoConfig?.extra?.MAPBOX_TOKEN;
+export const EXPO_PUBLIC_MAPBOX_TOKEN = Constants.expoConfig?.extra?.EXPO_PUBLIC_MAPBOX_TOKEN;
+export const EXPO_PUBLIC_SUPABASE_URL = Constants.expoConfig?.extra?.EXPO_PUBLIC_SUPABASE_URL;
+export const EXPO_PUBLIC_SUPABASE_ANON_KEY =
+  Constants.expoConfig?.extra?.EXPO_PUBLIC_SUPABASE_ANON_KEY;

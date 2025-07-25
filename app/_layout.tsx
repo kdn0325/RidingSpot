aecaf4609a672d7508a691a83b719be20ba11803
kdn0325 <dn10003@gmail.com>
2025-07-25 19:58:21 +0900

    feat: Ride Provider 추가

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,14 +3,17 @@ import { StatusBar } from 'react-native';
 import ScooterProvider from '~/providers/ScooterProvider';
 import AuthProvider from '~/providers/AuthProvider';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import RideProvider from '~/providers/RideProvider';
 
 export default function Layout() {
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
       <AuthProvider>
         <ScooterProvider>
-          <Stack screenOptions={{ headerShown: false }} />
-          <StatusBar barStyle={'light-content'} />
+          <RideProvider>
+            <Stack screenOptions={{ headerShown: false }} />
+            <StatusBar barStyle={'light-content'} />
+          </RideProvider>
         </ScooterProvider>
       </AuthProvider>
     </GestureHandlerRootView>

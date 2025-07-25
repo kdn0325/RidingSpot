95c9aa92d64035fbc81421dfdd5c4b402c0752a5
kdn0325 <dn10003@gmail.com>
2025-07-25 19:59:25 +0900

    feat: 라이딩 중 여부에 따른 스쿠터 마커 및 경로 표시 제어 추가

--- components/Map.tsx
+++ components/Map.tsx
@@ -5,12 +5,16 @@ import { useScooter } from '~/providers/ScooterProvider';
 import LineRoutes from './LineRoutes';
 import ScooterMarker from './ScooterMarker';
 import { EXPO_PUBLIC_MAPBOX_TOKEN } from '~/constant';
+import { useRide } from '~/providers/RideProvider';
 
 Mapbox.setAccessToken(EXPO_PUBLIC_MAPBOX_TOKEN || '');
+const customStyleURL = 'mapbox://styles/abelkim/cmdfs86wj03jd01r40yi091g2';
 
 export default function Map() {
   const { directionCoordinate } = useScooter();
-  const customStyleURL = 'mapbox://styles/abelkim/cmdfs86wj03jd01r40yi091g2';
+  const { ride } = useRide();
+
+  const showMarkers = !ride;
 
   return (
     <MapView
@@ -19,12 +23,15 @@ export default function Map() {
       styleURL={StyleURL.Dark}
       scaleBarEnabled={false}
       logoEnabled={false}
-      compassEnabled={false}
-      onDidFinishLoadingMap={() => console.log('Map loaded')}>
+      compassEnabled={false}>
       <Camera followZoomLevel={16} followUserLocation />
       <LocationPuck puckBearingEnabled puckBearing="heading" pulsing={{ isEnabled: true }} />
-      <ScooterMarker />
-      {directionCoordinate && <LineRoutes coordinates={directionCoordinate} />}
+      {showMarkers && (
+        <>
+          <ScooterMarker />
+          {directionCoordinate && <LineRoutes coordinates={directionCoordinate} />}
+        </>
+      )}
     </MapView>
   );
 }

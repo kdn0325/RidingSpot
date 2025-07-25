ed919a9d5896b97ca5cf1f771a28b44c4dc18908
kdn0325 <dn10003@gmail.com>
2025-07-25 16:36:15 +0900

    chore: 전역 레이아웃에 AuthProvider 적용

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,15 +1,18 @@
 import { Stack } from 'expo-router';
 import { StatusBar } from 'react-native';
 import ScooterProvider from '~/providers/ScooterProvider';
+import AuthProvider from '~/providers/AuthProvider';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
 export default function Layout() {
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
-      <ScooterProvider>
-        <Stack />
-        <StatusBar barStyle={'light-content'} />
-      </ScooterProvider>
+      <AuthProvider>
+        <ScooterProvider>
+          <Stack screenOptions={{ headerShown: false }} />
+          <StatusBar barStyle={'light-content'} />
+        </ScooterProvider>
+      </AuthProvider>
     </GestureHandlerRootView>
   );
 }
